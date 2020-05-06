ff6e0357f1d8048675634b822e6efd302bce0d98
Marek Machliński <marekin450@spoko.pl>
2020-05-06 13:04:32 +0200

    Extended test case.

--- src/test.cpp
+++ src/test.cpp
@@ -1,20 +1,30 @@
 #include "Net/Server.h"
 
+// args: TYPE(0=Classic, 1=Iterative) TOKEN HEIGHT WIDTH ZOOM OFFSET_X OFFSET_Y COLOR_OFFSET
 int main(int argc, char* argv[])
 {
     MPI::JobRequest request;
-    request.token = argc >= 2 ? abs(atoll(argv[1])) : 9999;
-    request.height = 2000;
-    request.width = 2000;
-    request.zoom = 2.0;
-    request.offset_x = -1.0;
-    uint64_t tok = 0;
+    request.token = argc >= 3 ? abs(atoll(argv[2])) : 9999;
+    request.height = argc >= 4 ? abs(atoi(argv[3])) : 2000;
+    request.width = argc >= 5 ? abs(atoi(argv[4])) : 2000;
+    request.zoom = argc >= 6 ? abs(atof(argv[5])) : 2.0;
+    request.offset_x = argc >= 7 ? abs(atof(argv[6])) : -1.0;
+    request.offset_y = argc >= 8 ? abs(atof(argv[7])) : 0.0;
+    request.color_offset = argc >= 9 ? abs(atoi(argv[8])) : 5;
     Net::Server serv("fractal_server.soc");
     serv.Listen(1);
     Net::Client soc("fractal_cluster.soc");
     sleep(5);
     soc.Connect();
-    soc.Write(MPI::MessageID::RequestClassic);
+    if (argc >= 2)
+    {
+        if (abs(atoi(argv[1])))
+            soc.Write(MPI::MessageID::RequestIterative);
+        else
+            soc.Write(MPI::MessageID::RequestClassic);
+    }
+    else
+        soc.Write(MPI::MessageID::RequestClassic);
     soc.Write(request);
     soc.Close();
     sleep(5);
@@ -22,7 +32,7 @@ int main(int argc, char* argv[])
     {
         soc = serv.Accept();
     } while (!soc.IsConnected());
-    soc.Read(tok);
+    soc.Read(request.token);
     soc.Close();
     serv.Close();
     Net::Client soc1("fractal_cluster.soc");
